e1e810a1148f2446ea6b5cb670b6897c2477ff4d
simranjeetkaur <chakkalsimmi@gmail.com>
2020-01-16 15:03:15 -0500

    Initial Commit

---
+++ coredata demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coredata demo
+//
+//  Created by Simran Chakkal on 2020-01-16.
+//  Copyright © 2020 Simran Chakkal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
